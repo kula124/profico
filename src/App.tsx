47e902f5034a0f62cef16fdcc0ef2b5849d9e98c
kula <kula124@gmail.com>
2021-10-04 20:19:08 +0200

    creating empty homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import React from 'react'
+import React, { FC } from 'react'
+import Homepage from 'pages/home'
 
-function App() {
+const App:FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-          <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Homepage />
+  )
 }
 
-export default App;
+export default App
