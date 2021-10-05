ee01e97df3ffdd65bd8d1a6f8a3d16dae4278ced
Ivan <kula124@gmail.com>
2021-10-05 18:56:36 +0200

    Feature/categories bar (#4)
    * adding const categories
    
    * creating inital categories bar solution
    
    * adding gray color to global style
    
    * adding styles to Category Item
    
    * using p instead of span; cleanup

---
+++ src/modules/categoriesBar/categories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+ 
+import { categories } from 'constants/categories'
+import CategoryItem from 'components/CategoryItem/cItem'
+
+import styles from './categories.module.scss'
+
+const CategoriesBar: React.FC = () => {
+  return (
+    <ul className={styles.main}>
+      {categories.map(c => <CategoryItem key={c.name}
+        {...c} />)}
+    </ul>
+  )
+}
+ 
+export default CategoriesBar
