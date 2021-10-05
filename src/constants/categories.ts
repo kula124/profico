1d7ce68f05bf293917da1343efd1f6f0d3bfc6cb
kula <kula124@gmail.com>
2021-10-05 18:31:30 +0200

    adding const categories

---
+++ src/constants/categories.ts
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import { ReactComponent as homeIcon } from '../assets/icons/Home.svg'
+import { ReactComponent as healthIcon } from '../assets/icons/Health.svg'
+import { ReactComponent as generalIcon } from '../assets/icons/News.svg'
+import { ReactComponent as scienceIco } from '../assets/icons/Science.svg'
+import { ReactComponent as sportsIcon } from '../assets/icons/Sports.svg'
+import { ReactComponent as technologyIcon } from '../assets/icons/TV-Guide.svg'
+import { ReactComponent as businessIcon } from '../assets/icons/Business.svg'
+
+
+
+export type CategoryName = 'home' | 'health' | 'technology' | 'general' | 'business' | 'science' | 'sports'
+
+export interface ICategory {
+  name: CategoryName,
+  icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+}
+
+export const categories:ICategory[] = [
+  {
+    icon: homeIcon,
+    name: 'home',
+  },
+  {
+    icon: generalIcon,
+    name: 'general',
+  },
+  {
+    icon: businessIcon,
+    name: 'business'
+  },
+  {
+    icon: healthIcon,
+    name: 'health',
+  },
+  {
+    icon: scienceIco,
+    name: 'science',
+  },
+  {
+    icon: sportsIcon,
+    name: 'sports',
+  },
+  {
+    icon: technologyIcon,
+    name: 'technology',
+  },
+] 
