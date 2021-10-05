11feea85ec940f86b089f5fb3f82b1e755286034
Ivan <kula124@gmail.com>
2021-10-05 16:54:53 +0200

    Feature/homepage prompt (#2)
    * adding image asset
    
    * extending BigButton
    
    * created home page prompt
    
    * adding home page prompt to the page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
+import HomePagePrompt from 'modules/homepagePrompt/hpPrompt'
 import SearchBar from 'modules/searchbar/searchbar'
 import React from 'react'
 
 const Homepage:React.FC = () => {
   return (
     <div>
+      <HomePagePrompt />
       <SearchBar />
     </div>
   )
