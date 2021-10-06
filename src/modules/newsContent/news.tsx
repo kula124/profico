bc7eda5e4835dded7060dd0ab8e01491ebfdf9e9
kula <kula124@gmail.com>
2021-10-06 10:39:55 +0200

    adding news container module

---
+++ src/modules/newsContent/news.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { testArray } from 'constants/newsItem'
+import NewsArticleThumb from 'components/NewsArticeThumb/newsThub'
+
+import styles from './news.module.scss'
+/* interface NewsContentProps {
+  
+} */
+ 
+const NewsContent: React.FC = () => {
+  return (
+    <section className={styles.main}>
+      <h2>News</h2>
+      <ul>
+        {testArray.map(e => <NewsArticleThumb {...e}
+          key={e.title} />)}
+      </ul>
+    </section>
+  )
+}
+ 
+export default NewsContent
