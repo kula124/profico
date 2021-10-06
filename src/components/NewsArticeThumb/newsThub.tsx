cc6108a5b37f912933ae5674cc117be9f485efb2
kula <kula124@gmail.com>
2021-10-06 14:45:48 +0200

    updating article thumbs

--- src/components/NewsArticeThumb/newsThub.tsx
+++ src/components/NewsArticeThumb/newsThub.tsx
@@ -1,17 +1,18 @@
+import { CategoryName } from 'constants/categories'
 import { INewsArticle } from 'constants/newsItem'
 import * as React from 'react'
 
 import styles from './thumb.module.scss'
  
-const NewsArticleThumb: React.FC<INewsArticle> = ({ author, title, urlToImage }) => {
+const NewsArticleThumb: React.FC<INewsArticle> = ({ author, title, category, urlToImage }) => {
   return (
     <li className={styles.item}>
       <img alt='article image'
         src={urlToImage} />
       <article>
-        <p>Tech</p>
+        <p>{category || 'general'}</p>
         <h1>{title}</h1>
-        <span>{author}</span>
+        <span>{author || 'author: n/a'}</span>
       </article>
     </li>
   )
