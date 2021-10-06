e86a283040cd114877befdfd184208b6726dcdfb
kula <kula124@gmail.com>
2021-10-06 14:51:34 +0200

    adding redirection on article click

--- src/components/NewsArticeThumb/newsThub.tsx
+++ src/components/NewsArticeThumb/newsThub.tsx
@@ -4,16 +4,20 @@ import * as React from 'react'
 
 import styles from './thumb.module.scss'
  
-const NewsArticleThumb: React.FC<INewsArticle> = ({ author, title, category, urlToImage }) => {
+const NewsArticleThumb: React.FC<INewsArticle> = ({ author, url, title, category, urlToImage }) => {
   return (
     <li className={styles.item}>
-      <img alt='article image'
-        src={urlToImage} />
-      <article>
-        <p>{category || 'general'}</p>
-        <h1>{title}</h1>
-        <span>{author || 'author: n/a'}</span>
-      </article>
+      <a href={url}
+        rel='noreferrer'
+        target='_blank' >
+        <img alt='article image'
+          src={urlToImage} />
+        <article>
+          <p>{category || 'general'}</p>
+          <h1>{title}</h1>
+          <span>{author || 'author: n/a'}</span>
+        </article>
+      </a>
     </li>
   )
 }
