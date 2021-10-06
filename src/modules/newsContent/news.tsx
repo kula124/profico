a3450be54d3312f3329296336a4f53b2b5825771
kula <kula124@gmail.com>
2021-10-06 13:37:01 +0200

    using the API to get data

--- src/modules/newsContent/news.tsx
+++ src/modules/newsContent/news.tsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { testArray } from 'constants/newsItem'
+import { INewsArticle } from 'constants/newsItem'
 import NewsArticleThumb from 'components/NewsArticeThumb/newsThub'
 
 import styles from './news.module.scss'
+import { getNewsByQuery } from 'utils/api'
 /* interface NewsContentProps {
   
 } */
  
 const NewsContent: React.FC = () => {
+  const [articles, setArticles] = useState<INewsArticle[] | void>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true)
+      const r = await getNewsByQuery({ q: 'tesla', limit: 10 })
+        .catch(() => setError(true))
+        .finally(() => setLoading(false))
+
+      setArticles(r)
+    }
+
+    fetch()
+  }, [])
+
+  if (error) {
+    return <div>ERROR!</div>
+  } 
+
   return (
     <section className={styles.main}>
       <h2>News</h2>
-      <ul>
-        {testArray.map(e => <NewsArticleThumb {...e}
+      {!loading && articles ? <ul>
+        {articles.map(e => <NewsArticleThumb {...e}
           key={e.title} />)}
         <div className={styles.tst}></div>
-      </ul>
+      </ul> : <div>Loading...</div>}
     </section>
   )
 }
