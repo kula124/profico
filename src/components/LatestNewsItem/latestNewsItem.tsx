59ee2195dec3be27914441c86cd89ba85e81b060
kula <kula124@gmail.com>
2021-10-07 22:45:31 +0200

    fixing time format

--- src/components/LatestNewsItem/latestNewsItem.tsx
+++ src/components/LatestNewsItem/latestNewsItem.tsx
@@ -1,15 +1,17 @@
-import { INewsArticle } from 'constants/newsItem'
 import * as React from 'react'
+import moment from 'moment'
+
+import { INewsArticle } from 'constants/newsItem'
 
 import styles from './latestNewsItem.module.scss'
  
-const LatestNewsItem: React.FunctionComponent<INewsArticle> = ({ url, title }) => {
+const LatestNewsItem: React.FunctionComponent<INewsArticle> = ({ url, title, publishedAt }) => {
   return (
     <li className={styles.main}>
       <a href={url}
         rel='noreferrer'
         target='_blank'>
-        <span>14:50</span>
+        <span>{moment(publishedAt).format('hh:mm')}</span>
         <h3>
           {title}
         </h3>
