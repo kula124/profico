145ba713fb573140a95544f134017b49fb797205
kula <kula124@gmail.com>
2021-10-08 21:20:19 +0200

    created switch module

---
+++ src/modules/MobileSpecific/Switch/switch.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+
+import styles from './switch.module.scss'
+
+export type Nav = 'f' | 'l' | ''
+
+const Switch: React.FC<{onSwitch: (a:Nav) => void}> = () => {
+  const [selected, setSelected] = useState<Nav>('')
+
+  const isSelected = (f:Nav):boolean => selected === f
+
+  useEffect(() => {
+    if (selected === '') {
+      setSelected('f')
+    }
+  }, [selected])
+
+  return (
+    <nav className={styles.main}>
+      <li className={styles[isSelected('f') ? 'selected' : '']}
+        onClick={() => setSelected('f')}
+      >
+        <span>Features</span>
+      </li>
+      <li className={styles[isSelected('l') ? 'selected' : '']}
+        onClick={() => setSelected('l')}
+      >
+        <span>Latest</span>
+      </li>
+    </nav>
+  )
+}
+ 
+export default Switch
