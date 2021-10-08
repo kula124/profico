8dc01b908a199e1902ab6748ab40befed4a7dcc4
kula <kula124@gmail.com>
2021-10-08 19:23:59 +0200

    moved to mobile (scss-vise)

--- src/modules/categoriesBar/categories.tsx
+++ src/modules/categoriesBar/categories.tsx
@@ -4,8 +4,15 @@ import { categories } from 'constants/categories'
 import CategoryItem from 'components/CategoryItem/cItem'
 
 import styles from './categories.module.scss'
+import { useMobile } from 'hooks/useMobile'
 
 const CategoriesBar: React.FC = () => {
+  const isMobile = useMobile()
+
+  if (isMobile) {
+    return null
+  }
+
   return (
     <ul className={styles.main}>
       {categories.map(c => <CategoryItem key={c.name}
