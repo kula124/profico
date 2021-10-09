43d57b1f95ff0ec8cb072cf62ba69d93c0b9cd80
kula <kula124@gmail.com>
2021-10-09 12:10:11 +0200

    created modal

---
+++ src/modules/MobileSpecific/Modal/modal.tsx
@@ -0,0 +1,16 @@
+import Logo from 'components/Logo/logo'
+import React from 'react'
+
+import styles from './modal.module.scss'
+
+const Modal: React.FC = () => {
+  
+  return (
+    <main className={styles.main}>
+      <Logo />
+      
+    </main>
+  ) 
+}
+ 
+export default Modal
