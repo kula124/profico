f61cd9351578b531ef24f7d1de733782648aad29
kula <kula124@gmail.com>
2021-10-09 12:34:32 +0200

    using portal for modal

--- src/modules/MobileSpecific/Modal/modal.tsx
+++ src/modules/MobileSpecific/Modal/modal.tsx
@@ -1,16 +1,29 @@
-import Logo from 'components/Logo/logo'
 import React from 'react'
+import { createPortal } from 'react-dom'
+
+import { modalRootName } from 'constants/consts'
+import Logo from 'components/Logo/logo'
+import Searchbox from 'components/Searchbox/searchbox'
+import CategoriesBar from 'modules/categoriesBar/categories'
 
 import styles from './modal.module.scss'
 
-const Modal: React.FC = () => {
+const Modal: React.FC<{isOpen: boolean, close: () => void}> = ({ isOpen, close }) => {
+  const portalHandle = document.getElementById(modalRootName)
+
+  if (!portalHandle) {
+    throw 'Portal handle not found!'
+  }
   
-  return (
+  return isOpen ? createPortal((
     <main className={styles.main}>
       <Logo />
-      
+      <p className={styles.close}
+        onClick={close}>x</p>
+      <Searchbox />
+      <CategoriesBar />
     </main>
-  ) 
+  ), portalHandle) : null
 }
  
 export default Modal
